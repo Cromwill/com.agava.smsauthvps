a655a9add8a3df676e14744620428773a3824deb
Kulachenko Aleks <hitsugayatru@gmail.com>
2025-05-07 13:44:23 +0300

    little fix

--- Runtime/Boot.cs
+++ Runtime/Boot.cs
@@ -81,6 +81,7 @@ namespace Agava.Wink
             }
             else
             {
+                _winkSignInHandlerUI.TrySetCorrectOrientation();
                 yield return _winkSignInHandlerUI.Initialize();
                 _loadingProgressBar.Disable();
                 _sceneLoader.LoadGameScene();

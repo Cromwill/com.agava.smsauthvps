8911fa145f69932fe799f3fe50e8b0e8cd5efe7b
Kulachenko Aleks <hitsugayatru@gmail.com>
2025-07-29 18:34:00 +0300

    fix fetch

--- Runtime/UIHandler/WindowPresenters/RewardContinueWindowPresenter.cs
+++ Runtime/UIHandler/WindowPresenters/RewardContinueWindowPresenter.cs
@@ -43,7 +43,7 @@ namespace Agava.Wink
         private Color _blinkTextColor;
         private Coroutine _reloadAd;
         private AppMetricaInfo _appMetricaInfo;
-        private int _fetchCount = 10;
+        private int _fetchCount = 3;
 
         public bool Initialized { get; private set; } = false;
 
